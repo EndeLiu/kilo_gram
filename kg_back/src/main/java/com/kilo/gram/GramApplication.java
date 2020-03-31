a83d15163c6385540574887534599bf19725ef58
LiuTao <45730683+EndeLiu@users.noreply.github.com>
2020-03-31 12:40:57 +0800

    空项目

---
+++ kg_back/src/main/java/com/kilo/gram/GramApplication.java
@@ -0,0 +1,13 @@
+package com.kilo.gram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GramApplication.class, args);
+    }
+
+}
---
+++ kg_back/src/test/java/com/kilo/gram/GramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kilo.gram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
